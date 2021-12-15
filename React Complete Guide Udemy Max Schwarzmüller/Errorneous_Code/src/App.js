1713412655a54ee45152d7fc7ca1dd7df3636cf3
Madhur Gupta <madhurgupta107@gmail.com>
2021-12-15 12:35:00 +0530

    code backup

--- React Complete Guide Udemy Max Schwarzmüller/Errorneous_Code/src/App.js
+++ React Complete Guide Udemy Max Schwarzmüller/Errorneous_Code/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
 import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
@@ -6,25 +6,24 @@ import "./App.css";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: "Do all exercises!", id: "g1" },
     { text: "Finish the course!", id: "g2" },
+    { text: "Do all exercises!", id: "g1" },
   ]);
 
   const addGoalHandler = (enteredText) => {
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: `g${updatedGoals.length}` });
-      console.log(prevGoals, "message",enteredText, "length", updatedGoals.length)
+      const index = updatedGoals.length;
+      updatedGoals.push({ text: enteredText, id: `g${index + 1}` });
+      console.log("message", enteredText, "index", index + 1);
       return updatedGoals;
     });
   };
 
   const deleteItemHandler = (goalId) => {
     setCourseGoals((prevGoals) => {
-      console.log("id", goalId, "prevgoals", prevGoals);
-      const updatedGoals = prevGoals.filter(
-        (goal) => goal.id !== `g${goalId + 1}`
-      );
+      console.log("id", goalId);
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
       return updatedGoals;
     });
   };
--- React Complete Guide Udemy Max Schwarzmüller/Errorneous_Code/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ React Complete Guide Udemy Max Schwarzmüller/Errorneous_Code/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
-import './CourseGoalList.css';
+import CourseGoalItem from "../CourseGoalItem/CourseGoalItem";
+import "./CourseGoalList.css";
 
-const CourseGoalList = props => {
+const CourseGoalList = (props) => {
   return (
     <ul className="goal-list">
-      {props.items.map((goal,index) => (
+      {props.items.map((goal) => (
         <CourseGoalItem
           key={goal.id}
           id={goal.id}
-          onDelete={()=>{props.onDeleteItem(index)}}
+          onDelete={() => {
+            props.onDeleteItem(goal.id);
+          }}
         >
           {goal.text}
         </CourseGoalItem>
