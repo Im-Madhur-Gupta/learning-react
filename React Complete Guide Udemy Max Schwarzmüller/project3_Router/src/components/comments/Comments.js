5bfe080390d50a32f8744befd9ad30eaf135b798
Madhur Gupta <madhurgupta107@gmail.com>
2022-01-25 14:46:34 +0530

    completes router final project

--- React Complete Guide Udemy Max Schwarzmüller/project3_Router/src/components/comments/Comments.js
+++ React Complete Guide Udemy Max Schwarzmüller/project3_Router/src/components/comments/Comments.js
@@ -1,25 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentsList from "./CommentsList";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const { quoteId } = useParams();
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  // jab bhi quoteId change ho to mujhe comments firse fetch karne padenge.
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const onAddedCommentHandler = () => {
+    // comment add karne pai mai new data fetch karna chahta hu.
+    sendRequest(quoteId);
+    console.log("added comment handler get");
+  };
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No Comments Found.</p>;
+  }
+
+  // we have our data
+  if (status === "completed" && loadedComments) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          onAddedComment={onAddedCommentHandler}
+          quoteId={quoteId}
+        />
+      )}
+      {comments}
     </section>
   );
 };
--- React Complete Guide Udemy Max Schwarzmüller/project3_Router/src/components/comments/NewCommentForm.js
+++ React Complete Guide Udemy Max Schwarzmüller/project3_Router/src/components/comments/NewCommentForm.js
@@ -1,26 +1,43 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
 
-import classes from './NewCommentForm.module.css';
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
+  const { sendRequest, status, error } = useHttp(addComment);
   const commentTextRef = useRef();
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      props.onAddedComment();
+    }
+  }, [status, error]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
